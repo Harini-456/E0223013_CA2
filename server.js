bcdb7fb65af443f3d3ff3f11df79090b121e328b
Harini-456 <harinisarav456@gmail.com>
2026-04-21 08:38:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = 6060;
+
+const BASE_URL = "https://t4e-testserver.onrender.com/api";
+
+let dataset = []
+const loadData = async => {
+    try{
+ const tokenResponse = await axios.post(`${BASE_URL}/public/token`,{
+            "studentId" : "E0223013",
+            "set":"setA",
+            "password":"495490"
+        });
+
+        const {token,dataUrl} = tokenResponse.data;
+
+
+        const dataResponse = await axios.get(`${BASE_URL}${dataUrl}`,{
+            
+            headers : {Authorization : `Bearer ${token}`},
+        });
+
+        dataset = dataResponse.data;
+
+        app.listen(PORT,() =>{
+            console.log(`Server is Running at ${PORT}`);
+        });
+    }
+catch(err){
+    console.log(err);
+}}
+
+loadData();
+
+
+
+app.get("/",(req,res)=>{
+    res.json(dataset);
+})
+
